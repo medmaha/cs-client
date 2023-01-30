53e8428661764165b0f7505ead83ab45572e0f76
= <=>
2023-01-30 01:51:39 +0000

    fiied bugs .... typescripts errors iiv

--- src/feeds/Feeds.tsx
+++ src/feeds/Feeds.tsx
@@ -1,11 +1,9 @@
 import React from "react"
-import ModernModal from "../components/UI/ModalCard"
-import PostContainer from "./posts"
+import PostContainer from "../apps/posts"
 
 export default function Feeds() {
     return (
         <div className="flex w-full max-w-[560px]">
-            {/* <ModernModal /> */}
             <PostContainer />
         </div>
     )
