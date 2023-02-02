35f8cbaa37b42c2a060a4607853548e5509f3cb2
= <=>
2023-02-02 04:38:11 +0000

    fiied bugs .... typescripts errors 2

--- pages/api/test.js
+++ pages/api/test.js
@@ -4,22 +4,25 @@ import axios from "axios"
 
 export default async function handler(req, res) {
     const form = { email: "admin@admin.com", password: "admin" }
-    await axios
-        .post("https://mahamedtoure.pythonanywhere.com/login", form, {
-            headers: { "Content-Type": "application/json" },
-        })
-        .then((_res) => {
-            const cookies = _res.headers["set-cookie"] || []
-            cookies.forEach((cookie) => {
-                console.log(cookie)
-                res.setHeader("Set-Cookie", cookie)
+
+    new Promise((resolve) => {
+        axios
+            .post("https://mahamedtoure.pythonanywhere.com/login", form, {
+                headers: { "Content-Type": "application/json" },
             })
-            Promise.resolve(
-                res.status(200).json({ Celesup: "Welcome to Celesup Api" }),
-            )
-        })
-        .catch((err) => {
-            console.log(err.message)
-        })
+            .then((_res) => {
+                const cookies = _res.headers["set-cookie"] || []
+                cookies.forEach((cookie) => {
+                    console.log(cookie)
+                    res.setHeader("Set-Cookie", cookie)
+                })
+                resolve(
+                    res.status(200).json({ Celesup: "Welcome to Celesup Api" }),
+                )
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+    })
 }
 
