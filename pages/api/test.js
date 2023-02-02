7395164dc223cf71f3c8c874209abd80f7884482
= <=>
2023-02-02 05:06:38 +0000

    fixes bugs ..:

--- pages/api/test.js
+++ pages/api/test.js
@@ -5,24 +5,23 @@ import axios from "axios"
 export default async function handler(req, res) {
     const form = { email: "admin@admin.com", password: "admin" }
 
-    new Promise((resolve) => {
-        axios
-            .post("https://mahamedtoure.pythonanywhere.com/login", form, {
-                headers: { "Content-Type": "application/json" },
+    await axios
+        .post("https://mahamedtoure.pythonanywhere.com/login", form, {
+            headers: { "Content-Type": "application/json" },
+        })
+        .then((_res) => {
+            const cookies = _res.headers["set-cookie"] || []
+            cookies.forEach((cookie) => {
+                console.log(cookie)
+                res.setHeader("Set-Cookie", cookie)
             })
-            .then((_res) => {
-                const cookies = _res.headers["set-cookie"] || []
-                cookies.forEach((cookie) => {
-                    console.log(cookie)
-                    res.setHeader("Set-Cookie", cookie)
-                })
-                resolve(
-                    res.status(200).json({ Celesup: "Welcome to Celesup Api" }),
-                )
-            })
-            .catch((err) => {
-                console.log(err.message)
-            })
-    })
+            res.status(200).json({ Celesup: "Welcome to Celesup Api" })
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+        .finally(() => {
+            Promise.resolve()
+        })
 }
 
